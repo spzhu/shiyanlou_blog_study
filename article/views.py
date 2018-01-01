5d3ff40400d87be47b6828e72b8a7a36bd77920f
943481660 <943481660@qq.com>
2018-01-01 17:20:22 +0800

    init commit

---
+++ article/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+def detail(request, args):
+    str = ("title = %s, category = %s, date_time = %s, content = %s" % 
+	(post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+def home(request):
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list': post_list})
